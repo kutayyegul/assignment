c56b8a9f7ed524625015e6b7a1819893291a357b
Kutay YEGÜL <kutay.yegul@hotmail.com>
2018-10-08 19:15:51 +0300

    asdasdasd

--- src/Test/Pages/page.py
+++ src/Test/Pages/page.py
@@ -1,4 +1,4 @@
-from locators import MainPageLocators
+from Pages.locators import MainPageLocators
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
